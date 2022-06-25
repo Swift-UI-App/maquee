059d7fc4629936093531ae19953424fb8b41746c
Createitv <xfy150150@163.com>
2022-06-25 15:30:53 +0800

    Initial Commit

---
+++ danmaku/danmakuApp.swift
@@ -0,0 +1,17 @@
+//
+//  danmakuApp.swift
+//  danmaku
+//
+//  Created by DifferThink on 6/25/22.
+//
+
+import SwiftUI
+
+@main
+struct danmakuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
